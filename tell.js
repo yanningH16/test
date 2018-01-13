a5b6c63e1f2dd72fac8542dba863d1d6f0ee3ae0
yanning <y_ning26@163.com>
2018-01-13 16:43:27 +0800

    node

--- tell.js
+++ tell.js
@@ -2,6 +2,6 @@ let express = require('express')
 let app = express()
 let server = require('http').createServer(app)
 
-app.use('/', express.static('./dist/index.html'))
+app.use(express.static('./dist'))
 
 server.listen(process.env.PORT || 8889)
