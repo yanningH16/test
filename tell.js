bd37761d875156ad7b8f0110e0a78d4cc889afd0
yanning <y_ning26@163.com>
2018-01-13 16:49:52 +0800

    单页面路由

--- tell.js
+++ tell.js
@@ -1,7 +1,10 @@
 let express = require('express')
 let app = express()
+var history = require('connect-history-api-fallback')
 let server = require('http').createServer(app)
-
+app.use(history({
+  index: './index.html'
+}))
 app.use(express.static('./dist'))
 
 server.listen(process.env.PORT || 8889)
